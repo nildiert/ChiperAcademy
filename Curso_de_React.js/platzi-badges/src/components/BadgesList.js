86a562b24a37dfb9feddc6f7003bb0f938f2579b
Nildiert <niljordan23@gmail.com>
2020-04-14 22:07:01 -0500

    Added api and get method

--- Curso_de_React.js/platzi-badges/src/components/BadgesList.js
+++ Curso_de_React.js/platzi-badges/src/components/BadgesList.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import './styles/BadgesList.css';
+import "./styles/BadgesList.css";
 
 class BadgesListItem extends React.Component {
   render() {
+
     return (
       <div className="BadgesListItem">
         <img
@@ -27,10 +29,20 @@ class BadgesListItem extends React.Component {
 
 class BadgesList extends React.Component {
   render() {
+    if (this.props.badges.length == 0) {
+      return (
+        <div>
+          <h3>No badges were found</h3>
+          <Link className="btn btn-primary" to="/badges/new">
+            Create new badge
+          </Link>
+        </div>
+      );
+    }
     return (
       <div className="BadgesList">
         <ul className="list-unstyled">
-          {this.props.badges.map(badge => {
+          {this.props.badges.map((badge) => {
             return (
               <li key={badge.id}>
                 <BadgesListItem badge={badge} />
