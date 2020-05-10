f7c7fe753b8e9a200fda49b40d1c63cc56593f19
Nildiert <niljordan23@gmail.com>
2020-05-09 21:49:36 -0500

    [FEATURE] Added BrowserRouter in main file (app.js)

--- Curso_de_React_Router/platzi-video-redux/src/entries/app.js
+++ Curso_de_React_Router/platzi-video-redux/src/entries/app.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import { render } from 'react-dom';
-import Home from '../pages/containers/home';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import reducer from '../reducers/index';
-import { Map as map } from 'immutable';
-import logger from 'redux-logger';
-import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import React from "react";
+import { render } from "react-dom";
+import Home from "../pages/containers/home";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reducer from "../reducers/index";
+import { Map as map } from "immutable";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { BrowserRouter } from "react-router-dom";
 // function logger({ getState, dispatch}) {
 //   return (next) => {
-//     return (action) => { 
+//     return (action) => {
 //       console.log('este es mi viejo estado', getState().toJS())
 //       console.log('vamos a enviar está acción', action);
 //       const value = next(action)
@@ -20,33 +21,27 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 //   }
 // }
 
-
-const logger_ = ({getState, dispatch }) => next => action => {
-  console.log('este es mi viejo estado', getState().toJS())
-  console.log('vamos a enviar está acción', action);
-  const value = next(action)
-  console.log('este es mi nuevo estado', getState().toJS())
-  return value
-}
+const logger_ = ({ getState, dispatch }) => (next) => (action) => {
+  console.log("este es mi viejo estado", getState().toJS());
+  console.log("vamos a enviar está acción", action);
+  const value = next(action);
+  console.log("este es mi nuevo estado", getState().toJS());
+  return value;
+};
 
 const store = createStore(
   reducer,
   map(),
-  composeWithDevTools(
-    applyMiddleware(
-      logger,
-      thunk
-    )
-  )
+  composeWithDevTools(applyMiddleware(logger, thunk))
 );
 
-
-const homeContainer = document.getElementById('home-container')
-
+const homeContainer = document.getElementById("home-container");
 
 render(
-  <Provider store={store}>
-    <Home />
-  </Provider>
-, homeContainer);
-
+  <BrowserRouter basename="/videos">
+    <Provider store={store}>
+      <Home />
+    </Provider>
+  </BrowserRouter>,
+  homeContainer
+);
