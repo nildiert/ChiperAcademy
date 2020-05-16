9fc0a4ec9bca48e4598faafef91440ebd20f053e
Nildiert <niljordan23@gmail.com>
2020-05-15 23:08:27 -0500

    [FEATURE] Handle history

--- Curso_de_React_Router/platzi-video-redux/src/pages/components/not-found.js
+++ Curso_de_React_Router/platzi-video-redux/src/pages/components/not-found.js
@@ -1,13 +1,33 @@
 import React, { PureComponent } from "react";
-import './generic-page.css'
+import "./generic-page.css";
 
 class NotFound extends PureComponent {
+  handleRandomClick = () => {
+    const random = Math.round(Math.random() * (10 - 1) + 1)
+    this.props.history.push(`/videos?id=${random}`, { id: random})
+  }
+  handleForwardClick = () => {
+    this.props.history.goForward();
+  };
+  handleBackClick = () => {
+    this.props.history.goBack();
+    // this.props.history.go(-1)
+  };
   render() {
     return (
       <div className="Page NotFound">
         <h1>404</h1>
         <h3 className="SadFace">:(</h3>
         <h2>No hemos encontrado la página que buscabas</h2>
+        <button className="Button" onClick={this.handleForwardClick}>
+          Ir a la siguiente página
+        </button>
+        <button className="Button" onClick={this.handleBackClick}>
+          Ir a la ruta anterior
+        </button>
+        <button className="Button" onClick={this.handleRandomClick}>
+          Video random
+        </button>
       </div>
     );
   }
