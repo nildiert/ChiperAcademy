6560ab10b841d210e64198c1b670177b43d80318
Nildiert <niljordan23@gmail.com>
2020-05-20 15:52:35 -0500

    [FEATURE] StaticRouter

--- Curso_de_React_Router/platzi-video-redux/src/entries/app.js
+++ Curso_de_React_Router/platzi-video-redux/src/entries/app.js
@@ -1,60 +1,15 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { render } from "react-dom";
-import Videos from "../pages/containers/videos";
-import Home from "../pages/components/home";
-import NotFound from "../pages/components/not-found";
-import Perfil from "../pages/components/perfil";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import reducer from "../reducers/index";
-import { Map as map } from "immutable";
-import logger from "redux-logger";
-import thunk from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
-import Header from "../pages/components/header";
-// function logger({ getState, dispatch}) {
-//   return (next) => {
-//     return (action) => {
-//       console.log('este es mi viejo estado', getState().toJS())
-//       console.log('vamos a enviar está acción', action);
-//       const value = next(action)
-//       console.log('este es mi nuevo estado', getState().toJS())
-//       return value
-//     }
-//   }
-// }
+import { BrowserRouter } from "react-router-dom";
+import App from '../pages/containers/app'
 
-const logger_ = ({ getState, dispatch }) => (next) => (action) => {
-  console.log("este es mi viejo estado", getState().toJS());
-  console.log("vamos a enviar está acción", action);
-  const value = next(action);
-  console.log("este es mi nuevo estado", getState().toJS());
-  return value;
-};
 
-const store = createStore(
-  reducer,
-  map(),
-  composeWithDevTools(applyMiddleware(logger, thunk))
-);
 
 const homeContainer = document.getElementById("home-container");
 
 render(
   <BrowserRouter>
-    <Provider store={store}>
-      <Fragment>
-        <Header />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/videos" component={Videos} />
-          <Route exact path="/perfil" component={Perfil} />
-          <Redirect from="/v" to="/videos" />
-          <Route component={NotFound} />
-        </Switch>
-      </Fragment>
-    </Provider>
+    <App/>
   </BrowserRouter>,
   homeContainer
 );
